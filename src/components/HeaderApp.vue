dcc969f9d81da5c24362401c7ace95c1c33e2aab
Julio Quintana <jquintana1801@gmail.com>
2017-12-14 14:15:32 -0400

    Optimizando codigo en el FirebasePlugin a ES6, modificando el package.json para salvar la dependencia de vuex

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -2,7 +2,7 @@
   <header class="navbar HeaderApp">
     <section class="navbar-section">
         <a href="#" class="navbar-brand mr-2">Todo APP</a>
-        <a href="https://github.com/picturepan2/spectre" class="btn btn-link">GitHub</a>
+        <a target="_blank" href="https://github.com/jqEmprendedorVE/vue-firestore-todolist" class="btn btn-link">GitHub</a>
     </section>
     <section class="navbar-section">
         <a href="#" class="navbar-brand mr-2 ocultar">Ingresar</a>
--- src/plugins/Firebase.js
+++ src/plugins/Firebase.js
@@ -3,26 +3,10 @@ import 'Firebase/firestore'
 import config from './config'
 
 var FirebasePlugin = {
-  config,
-  init: function () {
-    firebase.initializeApp(this.config)
-  },
-  db: function () {
-    if (firebase.apps.length === 0) this.init()
-    return firebase.database()
-  },
-  firestore: function () {
-    if (firebase.apps.length === 0) this.init()
-    return firebase.firestore()
-  },
-  auth: function () {
-    if (firebase.apps.length === 0) this.init()
-    return firebase.auth()
-  },
-  storage: function () {
-    if (firebase.apps.length === 0) this.init()
-    return firebase.storage()
-  },
+  db: () => firebase.database(),
+  firestore: () => firebase.firestore(),
+  auth: () => firebase.auth(),
+  storage: () => firebase.storage(),
   providers: {
     google: new firebase.auth.GoogleAuthProvider(),
     facebook: new firebase.auth.FacebookAuthProvider()
@@ -30,12 +14,6 @@ var FirebasePlugin = {
 }
 
 export default function (Vue) {
-  Vue.firebase = FirebasePlugin
-  Object.defineProperties(Vue.prototype, {
-    $firebase: {
-      get: function () {
-        return Vue.firebase
-      }
-    }
-  })
+  if (!firebase.apps.length) firebase.initializeApp(config)
+  Vue.prototype.$firebase = FirebasePlugin
 }
